d8dfcc38de88c7ed9b7e5ee1a855dd7bbaa124bc
Justin Thomas <57831354+justinthoms@users.noreply.github.com>
2020-09-26 12:53:16 +0530

    update

---
+++ app/config.py
@@ -0,0 +1,40 @@
+import traceback
+import sys
+import os
+
+
+try:
+    port = int(os.environ.get("PORT", "8080"))
+except ValueError:
+    port = -1
+if not 1 <= port <= 65535:
+    print("Please make sure the PORT environment variable is an integer between 1 and 65535")
+    sys.exit(1)
+
+try:
+    api_id = int(os.environ["API_ID"])
+    api_hash = os.environ["API_HASH"]
+except (KeyError, ValueError):
+    traceback.print_exc()
+    print("\n\nPlease set the API_ID and API_HASH environment variables correctly")
+    print("You can get your own API keys at https://my.telegram.org/apps")
+    sys.exit(1)
+
+try:
+    chat_id_raw = os.environ["CHAT_ID"].strip()
+    chat_ids = [int(chat_id.strip()) for chat_id in chat_id_raw.split(' ')]
+    alias_ids = []
+except (KeyError, ValueError):
+    traceback.print_exc()
+    print("\n\nPlease set the CHAT_ID environment variable correctly")
+    sys.exit(1)
+
+try:
+    session_string = os.environ["SESSION_STRING"]
+except (KeyError, ValueError):
+    traceback.print_exc()
+    print("\n\nPlease set the SESSION_STRING environment variable correctly")
+    sys.exit(1)
+
+host = os.environ.get("HOST", "0.0.0.0")
+debug = bool(os.environ.get("DEBUG"))
---
+++ app/routes.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+from aiohttp import web
+
+from .config import chat_ids, alias_ids
+
+
+def setup_routes(app, handler):
+    h = handler
+    routes =  [
+        web.get('/', h.home, name='home')
+    ]
+    for chat_id in chat_ids:
+        while True:
+            alias_id = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(len(str(chat_id)))])
+            if alias_id in alias_ids:
+                continue
+            alias_ids.append(alias_id)
+            break
+        p = f"/{alias_id}"
+        r = [
+            web.get(p, h.index),
+            web.get(p + r"/logo", h.logo),
+            web.get(p + r"/{id:\d+}/view", h.info),
+            web.get(p + r"/{id:\d+}/download", h.download_get),
+            web.head(p + r"/{id:\d+}/download", h.download_head),
+            web.get(p + r"/{id:\d+}/thumbnail", h.thumbnail_get),
+            web.head(p + r"/{id:\d+}/thumbnail", h.thumbnail_head),
+        ]
+        routes += r
+    app.add_routes(routes)
